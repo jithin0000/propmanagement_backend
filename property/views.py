4ec5607eb1d04ba76f40a042c26d569fc17c19d8
jithin0000 <mbjithinbabu@gmail.com>
2019-10-25 12:34:48 +0530

    filter added on property view

--- property/views.py
+++ property/views.py
@@ -15,7 +15,29 @@ class PropertyView(viewsets.ModelViewSet):
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        queryset = queryset
+
+        state = self.request.GET.get('state')
+        prop_type = self.request.GET.get('sale_type')
+        min_price = self.request.GET.get('min_price')
+        max_price = self.request.GET.get('max_price')
+
+
+        if state is not None:
+            state = int(state)
+            queryset = queryset.filter(state=state)
+        
+        if prop_type is not None:
+            queryset = queryset.filter(sale_type=prop_type)
+
+        if min_price is not None:
+            min_price = int(min_price)
+            queryset = queryset.filter(price__gt = min_price)
+        
+        if max_price is not None:
+            max_price = int(max_price)
+            queryset = queryset.filter(price__lt = max_price)
+        
+
         return queryset
 
 
